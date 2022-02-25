a35836cdc9dab925490c6e323400dc99c5c1d983
Kent F. Davis <k_davis@woodburycorp.com>
2022-02-25 15:09:50 -0700

    cleanup display

--- scripts/qo.sh
+++ scripts/qo.sh
@@ -6,8 +6,15 @@ if [[ "$#" -ne 0 ]]; then
 	exit 1
 else
 	if [[ -f scans/xml/_full_tcp_nmap.xml ]]; then
-		echo $(xmlstarlet select -t -v "count(/nmaprun/host/ports/port)" scans/xml/_full_tcp_nmap.xml) open ports.
-		echo -ne "PORT\tSERVICE\tPRODUCT\n"; for port in $(xmlstarlet select -t -v '/nmaprun/host/ports/port/@portid' scans/xml/_full_tcp_nmap.xml); do echo -ne "$port\t$(xmlstarlet select -t -v "/nmaprun/host/ports/port[@portid=$port]/service/@name" scans/xml/_full_tcp_nmap.xml) $(xmlstarlet select -t -v "/nmaprun/host/ports/port[@portid=$port]/service/@product" scans/xml/_full_tcp_nmap.xml)\n"; done
+		echo $(xmlstarlet select -t -v "count(/nmaprun/host/ports/port)" scans/xml/_full_tcp_nmap.xml) open TCP ports.
+		echo -ne "TCP PORT\tSERVICE\t\tPRODUCT\n"
+    for port in $(xmlstarlet select -t -v '/nmaprun/host/ports/port/@portid' scans/xml/_full_tcp_nmap.xml); do
+        if [[ "${#port}" -lt 4 ]]; then
+            echo -ne "    $port\t\t$(xmlstarlet select -t -v "/nmaprun/host/ports/port[@portid=$port]/service/@name" scans/xml/_full_tcp_nmap.xml)\t\t$(xmlstarlet select -t -v "/nmaprun/host/ports/port[@portid=$port]/service/@product" scans/xml/_full_tcp_nmap.xml)\n"
+        else
+            echo -ne "    $port\t$(xmlstarlet select -t -v "/nmaprun/host/ports/port[@portid=$port]/service/@name" scans/xml/_full_tcp_nmap.xml)\t\t$(xmlstarlet select -t -v "/nmaprun/host/ports/port[@portid=$port]/service/@product" scans/xml/_full_tcp_nmap.xml)\n"
+        fi
+    done
 		exit 0
 	else
 		echo -ne "\n[*]\tERROR: cannot find $PWD/scans/xml/_full_tcp_nmap.xml!\n[*]\tExiting...\n"
