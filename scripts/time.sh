23ba53358e4fd626b7d9f50f6fbb691843c62dcd
Kent F. Davis <kent.f.davis@gmail.com>
2022-03-03 13:13:19 -0500

    add timediff

---
+++ scripts/time.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if [ "$#" -ne 2 ]; then
+  echo -ne "\n[*]\tShell script to calculate how much time passed between start and end times.\n[*]\tUSAGE: $0 HH:MM HH:MM" >&2
+  exit 1
+fi
+
+START=$1
+FINISH=$2
+
+/usr/bin/dateutils.ddiff -f "%H hours %M minutes" $START $FINISH
+
