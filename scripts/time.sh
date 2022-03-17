a6d6335aaf1a1f457e70db41d7b43b90981111a3
Kent F. Davis <kent.f.davis@gmail.com>
2022-03-16 22:54:31 -0600

    sh to bash

--- scripts/time.sh
+++ scripts/time.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ "$#" -ne 2 ]; then
+if [[ "$#" -ne 2 ]]; then
   echo -ne "\n[*]\tShell script to calculate how much time passed between start and end times.\n[*]\tUSAGE: $0 HH:MM HH:MM" >&2
   exit 1
 fi
